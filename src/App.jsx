7b29860cd91782375a124fc44bf03166fce20db9
Vicktor007 <vicktord007@gmail.com>
2024-11-20 13:22:14 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react"
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
 import ExistingRooms from "./components/room/ExistingRooms"
